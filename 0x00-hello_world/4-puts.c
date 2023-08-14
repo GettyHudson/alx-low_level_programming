380efcfba206095576d2f4fa7f1bfaa1982a7341
GettyHudsonsec <ashigbig@gmail.com>
2023-08-14 16:18:21 -0700

    correct 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+/**
+ * main - Prints " Programming is like building a multilingual puzzle "
+ * Return: Returns 0
+ */
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
